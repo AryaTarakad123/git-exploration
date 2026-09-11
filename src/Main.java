d287f1274a718494b895700f3c5d1ac3502ee3c4
nsknishh <128nkumar@frhsd.com>
2026-09-11 09:12:24 -0400

    first reply

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    public static void main(String[] args) { System.out.println("AT"); }
+    public static void main(String[] args)
+    {
+        System.out.println("AT");
+        System.out.println("Hi AT, I am NSK");
+    }
 }
